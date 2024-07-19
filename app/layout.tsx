f873fe74da94b53acd1608da5c0279944f6b0bb3
Labesh <labheshwar.work@gmail.com>
2024-07-19 18:53:30 +0500

    basic setup and dependencies for next, typescript, tailwind and shadcn setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
-import "@/styles/globals.css"
 import { Inter as FontSans } from "next/font/google"
-
 import { cn } from "@/lib/utils"
+import "./globals.css"
+import { Metadata } from "next"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
+export const metadata: Metadata = {
+  title: 'LiveDocs',
+  description: 'Your go-to collaborative editor'
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        ...
+        {children}
       </body>
     </html>
   )
